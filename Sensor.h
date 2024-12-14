cea6c20ffbd3d8d7cf8356dcba7e6982bbfd255b
Bristine Muluh <bristinemuluh@gmail.com>
2024-12-14 16:10:22 -0600

    Fix linking issues with Sensor try 2

--- Sensor.h
+++ Sensor.h
@@ -32,23 +32,3 @@ public:
     Sensor(uint8_t cs, uint8_t miso, uint8_t mosi, uint8_t sck);
     ~Sensor();
 };
-
-Sensor::Sensor(uint8_t i2c_addr, uint8_t i2c_wire = I2C_WIRE0)
-: i2c_addr(i2c_addr)
-, i2c_wire(i2c_wire)
-{
-    sensor_mode = SENSOR_MODE_I2C;
-}
-
-Sensor::Sensor(uint8_t cs, uint8_t miso, uint8_t mosi, uint8_t sck)
-: spi_cs(cs)
-, spi_miso(miso)
-, spi_mosi(mosi)
-, spi_sck(sck)
-{
-    sensor_mode = SENSOR_MODE_SPI;
-}
-
-Sensor::~Sensor()
-{
-}
